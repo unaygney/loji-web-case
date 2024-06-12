c7278fb7cd67660d764bbf844663793f0461d544
Guney Unay <guneymarketing17@hotmail.com>
2024-06-12 22:17:41 +0300

    :bento: added logo asset

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,17 +1,6 @@
-import Image from "next/image";
+import { LogoIcon } from "./icons";
 import React from "react";
-import { Percent } from "lucide-react";
+
 export default function Logo() {
-  return (
-    // <Image
-    //   src={"/lojiper.png"}
-    //   alt="lojiper logo"
-    //   width={48}
-    //   height={48}
-    //   className="object-cover"
-    // />
-    <>
-      <Percent width={48} height={48} />
-    </>
-  );
+  return <LogoIcon />;
 }
